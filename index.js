b682efcde37f7b5468dd2e50fde4b186a8fdbef3
Vitaliy <kapron4ik@mail.ru>
2021-06-21 10:27:22 +0300

    change cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3010;
 
-app.use(cors())
+app.use(cors({origin: "http:\\localhost:3000"}))//поменять на расшаренную страницу портфолио { origin: "httpы:\\safronman.github.io" }
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
