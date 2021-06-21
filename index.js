08c5c13fe62d7f52072e91d869cabdce3c01ae67
Vitaliy <kapron4ik@mail.ru>
2021-06-21 13:06:32 +0300

    change cors

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const port = process.env.PORT || 3010;
 
 const corsOptions ={
     // origin:'http://localhost:3000',
-    origin:'https://kapron4ik.github.io/IT_INCUBATOR_PORTFOLIO/',
+    origin:['https://kapron4ik.github.io/IT_INCUBATOR_PORTFOLIO/'],
+    methods: "GET,PUT,POST,DELETE",
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
